986b266e983265bea5c9d6499dac6333339b9b44
Steven Shi <stevendiwenshi1113@gmail.com>
2021-02-27 00:42:50 -0800

    ngl I forgot like half the stuff I added, prototype teleportation, gravity fix, lighting fix, idk

---
+++ Non-Eucliedian Game/Assets/Scripts/TeleportController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportController : MonoBehaviour
+{
+    BoxCollider col;
+    public Vector3 offset;
+    public Color gizmoColor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        col = GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = gizmoColor;
+        Gizmos.DrawCube(transform.position, new Vector3(0.5f, 0.5f, 0.5f));
+        Gizmos.DrawCube(transform.position + offset, new Vector3(0.5f, 0.5f, 0.5f));
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.name);
+       
+        if (other.CompareTag("Player"))
+        {
+            CharacterController cc = other.GetComponent<CharacterController>();
+            cc.enabled = false;
+            other.transform.position += offset;
+            cc.enabled = true;
+            
+        }
+    }
+}

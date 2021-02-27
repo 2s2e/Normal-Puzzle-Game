99dfe7f81a2f936fedef726e952087251b84e245
Steven Shi <stevendiwenshi1113@gmail.com>
2021-02-26 20:11:37 -0800

    Added assets folder

---
+++ Non-Eucliedian Game/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    [Range(0f, 200f)]
+    public float MouseSenstivity = 100f;
+    public Transform playerBody;
+
+    float xRotation = 0f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * MouseSenstivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * MouseSenstivity * Time.deltaTime;
+        playerBody.Rotate(Vector3.up * mouseX); //Move up
+        
+        xRotation -= mouseY; // minues because otherwise will be flipped
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0); //Move side;
+    }
+}

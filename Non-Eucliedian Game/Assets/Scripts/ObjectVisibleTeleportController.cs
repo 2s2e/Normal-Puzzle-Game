11bbcc448e7a63b69682f99c8c9b1e86b03fa858
Steven Shi <stevendiwenshi1113@gmail.com>
2021-02-27 21:42:54 -0800

    Began working on forest level along with other quality of life changes

---
+++ Non-Eucliedian Game/Assets/Scripts/ObjectVisibleTeleportController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectVisibleTeleportController : MonoBehaviour
+{
+    BoxCollider col;
+    public Vector3 offset;
+    public Color gizmoColor;
+    public GameObject[] nonVisibles;
+    // Start is called before the first frame update
+    void Start()
+    {
+        col = GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = gizmoColor;
+        Gizmos.DrawCube(transform.position, new Vector3(0.5f, 0.5f, 0.5f));
+        Gizmos.DrawCube(transform.position + offset, new Vector3(0.5f, 0.5f, 0.5f));
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log(other.name);
+
+        if (other.CompareTag("Player"))
+        {
+            foreach(GameObject obj in nonVisibles)
+            {
+                Renderer m_renderer = obj.GetComponent<Renderer>();
+                if(m_renderer.isVisible)
+                {
+                    return;
+                }
+            }
+            CharacterController cc = other.GetComponent<CharacterController>();
+            cc.enabled = false;
+            other.transform.position += offset;
+            cc.enabled = true;
+
+        }
+    }
+}
--- Non-Eucliedian Game/Assets/Scripts/PlayerMovement.cs
+++ Non-Eucliedian Game/Assets/Scripts/PlayerMovement.cs
@@ -13,49 +13,25 @@ public class PlayerMovement : MonoBehaviour
 
     Vector3 velocity; 
 
-    public Transform groundCheck;
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
     bool isGrounded;
-
-    public LayerMask infiniteMask;
-    bool isInf;
+    public Transform groundCheck;
     void Update()
     {
-        //Moves the player, shift to move faster
-        if(Input.GetKey(KeyCode.LeftShift)) {
-            speed = 20f;
-        } else {
-            speed = 12f;
-        }
-        //If in infinite Hallway, then running slows down you instead
-        isInf = Physics.CheckSphere(groundCheck.position, groundDistance, infiniteMask);
-        if(isInf) {
-            if(Input.GetKey(KeyCode.LeftShift)) {
-                speed = 5f;
-                Debug.Log("slowing");
-            }
-            Debug.Log("touching");
-        }
+        
+        
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
         Vector3 move = transform.right * x + transform.forward * z; 
         controller.Move(move * speed * Time.deltaTime);
-      
+        
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
         //Implements Jumping
         if(Input.GetButtonDown("Jump") && isGrounded) {
             velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
         }
-
-        if(Input.GetKeyDown(KeyCode.Y))
-        {
-            Debug.Log("Bruh");
-            transform.Translate(5, 5, 5);
-        }
-
-        //Implements gravity
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
-        if(isGrounded && velocity.y < 0){
+        if(isGrounded && velocity.y < 0) {
             velocity.y = -2f;
         }
         velocity.y += gravity * Time.deltaTime; 
